f287987125b6010c8c5ed68efc37a9557f441096
michaelhans <michaelhans777@gmail.com>
2020-11-05 23:31:12 +0700

    Clean the repository

--- src/Reader.py
+++ src/Reader.py
@@ -1,25 +1,43 @@
-import Process
+from Multiversion import *
 import re
 
-class Reader:
+# General setup for transactions and data
+def generalSetup(fileName):
+    file = open("../test/" + fileName, "r")
+    buff = file.read()
+    arrString = buff.split('\n')
 
-    def __init__(self, fileName):
-        self.arrProcess = []
-        self.arrTransaction = []
-        self.convertString(fileName)
+    # Setup all transaction for Concurrency Control
+    arrTransaction = []
+    num_of_transaction = int(arrString.pop(0))
+    for i in range(num_of_transaction):
+        arrTransaction.append(Transaction(i+1))
 
-    def convertString(self, fileName):
-        file = open("../test/" + fileName, "r")
-        buff = file.read()
-        arrString = buff.split('\n')
-        for string in arrString:
-            print(string)
-            actionID = string[0]
-            transactionID = re.findall('[0-9]+', string)[0]
-            data = string.split('(')[1]
-            print(actionID)
-            print(transactionID)
-            print(data[0: len(data)-1])
+    # Setup all needed data for Concurrency Control
+    arrData = []
+    raw_data = arrString.pop(0).split(' ')
+    for item in raw_data:
+        arrData.append(Data(item))
+    return arrTransaction, arrData, arrString
 
-if __name__ == "__main__":
-    S = Scheduler("soal_1.txt")
+# Simple Locking (Exclusive Lock Only) Concurrency Control Converter
+def SLock_Converter(arrTransaction, arrData, arrString):
+    pass
+
+# Serial Optimistic Concurrency Control Converter
+def OCC_Converter(arrTransaction, arrData, arrString):
+    pass
+
+# Multiversion Timestamp Ordering Concurrency Control Converter
+def MVCC_Converter(arrTransaction, arrData, arrString):
+    MVCC_DataMap = DataMap(arrData)
+    MVCC_Process = []
+    for string in arrString:
+        transaction_id = int(re.findall('[0-9]+', string)[0])
+        action = string[0]
+        raw_data = string.split('(')[1]
+        dataLabel = raw_data[0: len(raw_data)-1]
+        newProcess = Process(arrTransaction[transaction_id - 1], action, Data(dataLabel), MVCC_DataMap)
+        MVCC_Process.append(newProcess)
+    
+    return MVCC_DataMap, MVCC_Process
